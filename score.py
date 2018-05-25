e2aea6a1642136829b9709f58bfac56332b7872a
tylerhutcherson <tyler.hutcherson@metismachine.com>
2018-05-25 12:31:21 -0400

    first commit

---
+++ score.py
@@ -0,0 +1,50 @@
+import time
+import warnings
+import pandas as pd
+from helpers.logger import get_logger
+from helpers.modeling import load_latest_model
+from helpers.data import normalize_gender, batches
+from helpers.schema import PREDICTION_SCHEMA, FEATURES, OUTPUT
+from skafossdk import DataSourceType, Skafos
+warnings.filterwarnings("ignore")
+
+
+## Load the most recent noshow model that has been pre-trained
+start = time.time()
+log = get_logger('no-show-scoring')
+ska = Skafos()
+log.info("Loading latest pre-trained no-show predictor")
+latest_model = load_latest_model(engine=ska.engine)
+
+
+## Pull in upcoming appts from no_shows keyspace
+log.info("Loading upcoming appointments")
+ska.engine.create_view("upcoming_appointments",
+    {"keyspace": "no_shows", "table": "upcoming"},
+    DataSourceType.Cassandra).result()
+upcoming = ska.engine.query("SELECT * FROM upcoming_appointments").result().get('data')
+log.info("Loaded {} upcoming appointments to score".format(len(upcoming)))
+
+
+## Load data to a pandas dataframe and perform some normalization steps
+log.info("Prepping data for scoring")
+df = normalize_gender(pd.DataFrame(upcoming))
+X = df[FEATURES]
+
+
+## Score the batch of appointments
+log.info("Scoring all appointments")
+df['no_show_likelihood'] = [p[1] for p in latest_model.predict_proba(X)]
+
+## Create prediction records
+predictions = df[OUTPUT]
+
+
+## Write predictions to cassandra table - accessible via REST API
+log.info("Writing batches of predictions out to the database")
+for batch in batches(predictions, 50):
+  res = ska.engine.save(PREDICTION_SCHEMA, batch).result()
+  log.debug(res)
+
+finish = time.time()
+log.info(f"Done - loaded and scored appointments in {finish-start} seconds")
---
+++ train.py
@@ -0,0 +1,55 @@
+import os
+import uuid
+import warnings
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+from skafossdk import *
+from helpers.logger import get_logger
+from helpers.schema import MODEL_SCHEMA
+from helpers.modeling import save_model
+from helpers.data import fetch_data
+warnings.filterwarnings("ignore")
+
+
+TEST_SIZE = float(os.getenv('TEST_SIZE', 0.2))
+log = get_logger('no-show-training')
+ska = Skafos()
+
+## Grab data using the Skafos data engine
+log.info("Fetching historical appointment data over a 3 month range")
+X, y = fetch_data(engine=ska.engine)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=42)
+
+
+## Build model on training data
+# NOTE: Perform more feature and hyperparameter tuning
+log.info("Building a basic random forest classifier with balanced classes")
+rf = RandomForestClassifier(class_weight='balanced')
+rf.fit(X_train, y_train)
+
+
+## Evaluate basic model performance
+log.info("Checking basic model performance on some test data")
+y_preds = rf.predict(X_test)
+y_scores = [p[1] for p in rf.predict_proba(X_test)]
+model_accuracy = accuracy_score(y_test, y_preds)
+model_auc = roc_auc_score(y_test, y_scores)
+log.info(f"Accuracy: {model_accuracy} \n ROC_AUC: {model_auc}")
+
+
+## Refit final model
+log.info("Training final model")
+rf_final = RandomForestClassifier(class_weight='balanced')
+rf_final.fit(X, y)
+
+
+## Save Model to s3 and store a record in the database to access most recent model
+log.info("Dumping trained model to s3 and saving a model version record in cassandra")
+model_id = str(uuid.uuid4())
+record = save_model(model=rf_final, model_id=model_id, acc=model_accuracy,
+    model_type="RandomForestClassifier", name="no_show_model")
+
+ska.engine.save(MODEL_SCHEMA, [record]).result()
+
+log.info(f"Done - saved out trained no-show model '{model_id}'")
