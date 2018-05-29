483e8ff0951ec3103dff354407a70cd7fd43b29c
tylerhutcherson <tyler.hutcherson@metismachine.com>
2018-05-29 08:13:11 -0400

    increase the bacth size

--- score.py
+++ score.py
@@ -44,7 +44,7 @@ print(predictions[:3], flush=True)
 
 ## Write predictions to cassandra table - accessible via REST API
 log.info("Writing batches of predictions out to the database")
-for batch in batches(predictions, 50):
+for batch in batches(predictions, 1000):
   res = ska.engine.save(PREDICTION_SCHEMA, batch).result()
   log.debug(res)
 
