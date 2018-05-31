be844b8a81ed9da6436f7dd791e2cd406cfffed3
tylerhutcherson <tyler.hutcherson@metismachine.com>
2018-05-31 13:03:51 -0400

    update to use text

--- helpers/schema.py
+++ helpers/schema.py
@@ -26,8 +26,8 @@ PREDICTION_SCHEMA = {
     "order_by": ["no_show_likelihood desc"]
   },
   "columns": {
-    "appointment_id": "string",
-    "patient_id": "string",
+    "appointment_id": "text",
+    "patient_id": "text",
     "appointment_day": "date",
     "no_show_likelihood": "float"
   }
